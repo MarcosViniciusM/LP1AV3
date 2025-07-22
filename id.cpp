5199bc7301bee6ca166257c9a221b09ada9aba89
MarcosViniciusM <marcos.vinicius.ferreira.700@ufrn.edu.br>
2025-07-22 19:07:05 -0300

    Comm id.cpp
    Comentários

--- id.cpp
+++ id.cpp
@@ -2,7 +2,8 @@
 #include "id.h"
 using namespace std;
 
-
+// Quando essa função é chamada, é assinalado um ID crescente, independente da classe que invocou
+// Feita desta maneira para criar uma lista de IDs uniforme, já que as classes tem seus respectivos vetores
 int getNextId(){
 	static int nextId=0;
 	nextId++;
