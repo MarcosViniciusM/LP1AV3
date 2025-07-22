aebe4db4f94db480011c11709706d54224ce176d
MarcosViniciusM <marcos.vinicius.ferreira.700@ufrn.edu.br>
2025-07-22 19:15:43 -0300

    Comm main.cpp
    Comentários

--- main.cpp
+++ main.cpp
@@ -18,16 +18,22 @@ int main(){
 	lerImovel(vImovel);
 	
 	int numAval=numAvaliadores(vCorretor);
-	
+
+	// Avaliador 
 	for(int i=0;i<vCorretor.size();i++){
+		//Pula o processo de visitas, se o corretor não for um avaliador
 		if(vCorretor[i].getAvaliador()==0){
 			continue;
 		}
+		// Unicamente para encaixar perfeitamente com as saídas
 		if(i){
 			cout << endl;
 		}
 		vector<int> rota;
+		// Coloca o trajeto a ser feito no vetor rota
 		criarRota(vImovel, vCorretor[i], rota, numAval);
+		
+		// Imprime o trajeto, e o horário
 		imprimirRota(vImovel, vCorretor[i], rota);
 	}
 	
